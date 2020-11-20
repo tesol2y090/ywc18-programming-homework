f7567155a1190368928912a6efc71b588800d4e9
Gang (Kantapat) <gliese090@gmail.com>
2020-11-21 01:40:44 +0700

    Deploy gh-pages

---
+++ _next/static/Z2h51mhEOsJXerFE5XWHB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-b7e844aa5a9113d484e7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4df5858c1c1adeb3d207.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
